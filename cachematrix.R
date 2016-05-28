333541634aa3c3072006bb36a9fb5ca2a936b443
rythmdivine2 <rythmdivine2@gmail.com>
2016-05-29 02:44:45 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,19 @@
 
 ## Write a short comment describing this function
 
+## This function creates a special "matrix" object 
 makeCacheMatrix <- function(x = matrix()) {
-
+ inv_m <- NULL
+  set <- function(y){
+    x <<- y
+    inv_m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv_m <<- inverse
+  getInverse <- function() inv_m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
@@ -12,4 +23,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv_m <- x$getInverse()
+  if(!is.null(inv_m))
+  {
+    message("Cached data")
+    return(inv_m)
+  }
+  data <- x$get()
+  if(nrow(data)==ncol(data)) {
+    #unlist(determinant(data))
+    if(det(data)==0){
+      message("Determinant Value is 0, Matrix not invertible !!")
+    } else {
+      inv_m <- solve(data)
+      x$setInverse(inv_m)
+      inv_m
+    }
+  } else {
+    message("Not a square matrix, Matrix not invertible !! ")
+  }
 }
